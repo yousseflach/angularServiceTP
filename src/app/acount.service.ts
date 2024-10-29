991c75158f97f9e20e99ed75ebcb0a12fa270fd7
yousseflach <lachgar.youssef6174@gmail.com>
2024-10-29 14:25:49 +0100

    creating acount service

---
+++ src/app/acount.service.ts
@@ -0,0 +1,18 @@
+export class AcountService {
+    acounts: {title: string, status: string}[] = [
+        { title: 'element1', status: 'active'},
+        { title: 'element2', status: 'inActive'},
+        { title: 'element3', status: 'unknown'},
+        { title: 'element4', status: 'inActive'},
+        { title: 'element5', status: 'inActive'},
+        { title: 'element6', status: 'active'},
+        { title: 'element7', status: 'inActive'},
+        { title: 'element8', status: 'unknown'},
+        { title: 'element9', status: 'inActive'},
+        { title: 'element10', status: 'inActive'}
+    ];
+
+    changeStatus(status: string, id: number){
+        this.acounts[id].status = status;
+    }
+}
